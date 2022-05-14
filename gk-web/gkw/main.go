b5e4aeaeb11c42840bc15733164480b9afaf90bd
stream1080 <fmxhax638@foxmail.com>
2022-05-14 18:26:14 +0800

    feat: 实现 ServeHTTP 方法

--- gk-web/gkw/main.go
+++ gk-web/gkw/main.go
@@ -6,19 +6,22 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/index", indexHandler)
-	http.HandleFunc("/hello", helloHandler)
+type Engine struct{}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func indexHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
-}
-
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	for k, v := range req.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/index":
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	case "/hello":
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
 	}
+}
+
+func main() {
+	engine := new(Engine)
+	log.Fatal(http.ListenAndServe(":8080", engine))
 }
