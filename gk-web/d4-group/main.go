e03e375377bf5a7dd821463f763300e361a3a6d1
stream1080 <fmxhax638@foxmail.com>
2022-07-30 17:23:39 +0800

    feat: 路由分组

--- gk-web/d4-group/main.go
+++ gk-web/d4-group/main.go
@@ -10,27 +10,37 @@ func main() {
 	r := gkw.New()
 
 	r.GET("/index", func(c *gkw.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		c.HTML(http.StatusOK, "<h1>Index Pages</h1>")
 	})
 
-	r.GET("/hello", func(c *gkw.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-
-	r.GET("/hello/:name", func(c *gkw.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *gkw.Context) {
-		c.JSON(http.StatusOK, gkw.H{"filepath": c.Param("filepath")})
-	})
-
-	r.POST("/login", func(c *gkw.Context) {
-		c.JSON(http.StatusOK, gkw.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gkw.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello V1 Pages</h1>")
 		})
-	})
+
+		v1.GET("/hello", func(c *gkw.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gkw.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+
+		v2.GET("/assets/*filepath", func(c *gkw.Context) {
+			c.JSON(http.StatusOK, gkw.H{"filepath": c.Param("filepath")})
+		})
+
+		v2.POST("/login", func(c *gkw.Context) {
+			c.JSON(http.StatusOK, gkw.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
 
 	r.Run(":8080")
 }
