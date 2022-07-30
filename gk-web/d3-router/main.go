a6e29b8d9d2dc4b688eae3e2da284f0967b578b0
stream1080 <fmxhax638@foxmail.com>
2022-07-30 16:50:40 +0800

    feat: 增加动态路由

--- gk-web/d3-router/main.go
+++ gk-web/d3-router/main.go
@@ -17,6 +17,14 @@ func main() {
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
+	r.GET("/hello/:name", func(c *gkw.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *gkw.Context) {
+		c.JSON(http.StatusOK, gkw.H{"filepath": c.Param("filepath")})
+	})
+
 	r.POST("/login", func(c *gkw.Context) {
 		c.JSON(http.StatusOK, gkw.H{
 			"username": c.PostForm("username"),
