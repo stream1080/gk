205de3025990ef63f9572dd8fa824f1b10a031d7
stream1080 <fmxhax638@foxmail.com>
2022-07-31 14:07:38 +0800

    feat: d7 init

---
+++ gk-web/d7-panic-recover/gkw/context.go
@@ -0,0 +1,93 @@
+package gkw
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type H map[string]interface{}
+
+type Context struct {
+	Writer     http.ResponseWriter // origin objects
+	Req        *http.Request       //
+	Path       string              // request info
+	Method     string              //
+	Params     map[string]string   //
+	StatusCode int                 // response info
+	handlers   []HandlerFunc       // middleware
+	index      int                 //
+	engine     *Engine             // engine pointer
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+		index:  -1,
+	}
+}
+
+func (c *Context) Next() {
+	c.index++
+	s := len(c.handlers)
+	for ; c.index < s; c.index++ {
+		c.handlers[c.index](c)
+	}
+}
+
+func (c *Context) Fail(code int, err string) {
+	c.index = len(c.handlers)
+	c.JSON(code, H{"message": err})
+}
+
+func (c *Context) Param(key string) string {
+	return c.Params[key]
+}
+
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+func (c *Context) HTML(code int, name string, data interface{}) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	if err := c.engine.htmlTemplates.ExecuteTemplate(c.Writer, name, data); err != nil {
+		c.Fail(500, err.Error())
+	}
+}
---
+++ gk-web/d7-panic-recover/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+
+	"gkw"
+)
+
+func main() {
+	r := gkw.New()
+
+	r.Use(gkw.Logger())
+
+	r.GET("/index", func(c *gkw.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Pages</h1>", nil)
+	})
+
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gkw.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello V1 Pages</h1>", nil)
+		})
+
+		v1.GET("/hello", func(c *gkw.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+
+	v2 := r.Group("/v2")
+	v2.Use(onlyForV2())
+	{
+		v2.GET("/hello/:name", func(c *gkw.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+
+		v2.GET("/assets/*filepath", func(c *gkw.Context) {
+			c.JSON(http.StatusOK, gkw.H{"filepath": c.Param("filepath")})
+		})
+
+		v2.POST("/login", func(c *gkw.Context) {
+			c.JSON(http.StatusOK, gkw.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
+
+	// 静态文件访问
+
+	stu1 := &student{Name: "Pony", Age: 20}
+	stu2 := &student{Name: "Jack", Age: 22}
+
+	r.Static("/assets", "./static")
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", func(c *gkw.Context) {
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+
+	r.GET("/students", func(c *gkw.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", gkw.H{
+			"title":  "gkw",
+			"stuArr": [2]*student{stu1, stu2},
+		})
+	})
+
+	r.GET("/date", func(c *gkw.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", gkw.H{
+			"title": "gkw",
+			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
+		})
+	})
+
+	r.Run(":8080")
+}
+
+func onlyForV2() gkw.HandlerFunc {
+	return func(c *gkw.Context) {
+		// Start timer
+		t := time.Now()
+		// if a server error occurred
+		c.Fail(500, "Internal Server Error")
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
+
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
