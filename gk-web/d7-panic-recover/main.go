ee5b11215d33f7078506b704809c32d8d95092cc
stream1080 <fmxhax638@foxmail.com>
2022-07-31 15:03:59 +0800

    feat: panic demo

--- gk-web/d7-panic-recover/main.go
+++ gk-web/d7-panic-recover/main.go
@@ -78,6 +78,13 @@ func main() {
 		})
 	})
 
+	r.Use(gkw.Recovery())
+	// index out of range for testing Recovery()
+	r.GET("/panic", func(c *gkw.Context) {
+		names := []string{"geektutu"}
+		c.String(http.StatusOK, names[100])
+	})
+
 	r.Run(":8080")
 }
 
